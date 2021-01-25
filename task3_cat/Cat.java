3977ee8e08043fb052ab5594d5e8934d887ba4b4
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-01-25 11:07:21 +0300

    add do-while

--- task3_cat/Cat.java
+++ task3_cat/Cat.java
@@ -4,6 +4,11 @@ public class Cat {
     private int catAge = 5;
     private String catName = "mr.Meowster";
 
+    public void CatConstr(int catAge, String catName) {
+        this.catAge = catAge;
+        this.catName = catName;
+    }
+
     public int getCatAge() {
         return catAge;
     }
@@ -20,26 +25,36 @@ public class Cat {
         this.catName = catName;
     }
 
-    public void tenCatsWithDefaultFields(){
+    public void tenCatsWithDefaultFields() {
 
         int counter = 1;
-        while (counter<=10){
-            System.out.println(counter+".Cat Name: "+getCatName()+"\n  Cat Age: "+getCatAge());
+        while (counter <= 10) {
+            System.out.println(counter + ".Cat Name: " + getCatName() + "\n  Cat Age: " + getCatAge());
             counter++;
         }
     }
 
-    public void tenCatsWithFori(){
+    public void tenCatsWithFori() {
         for (int i = 0; i < 10; i++) {
-            setCatName("Meowler"+Integer.toString(i));
-            setCatAge(1+i);
-            System.out.println(i+".Cat Name: "+getCatName()+"\n  Cat Age: "+getCatAge());
+            setCatName("Meowler" + Integer.toString(i));
+            setCatAge(1 + i);
+            System.out.println(i + ".Cat Name: " + getCatName() + "\n  Cat Age: " + getCatAge());
 
         }
     }
 
+    public void tenCatsDoWhile() {
+        CatConstr(10, "Dawg");
+        int counter = 1;
+        do {
+            System.out.println(counter+". "+getCatName()+" "+getCatAge()+"\n");
+            counter++;
+        }   while (counter<=10);
+
+    }
+
     @Override
     public String toString() {
-        return "Cat Name: "+getCatName()+"\nCat  Age: "+getCatAge();
+        return "Cat Name: " + getCatName() + "\nCat  Age: " + getCatAge();
     }
 }
--- task3_cat/MainClass.java
+++ task3_cat/MainClass.java
@@ -3,12 +3,10 @@ package task3_cat;
 public class MainClass {
     public static void main(String[] args) {
         Cat cat = new Cat();
-//        cat.setCatAge(12);
-//        cat.setCatName("Meow");
-//        System.out.println(cat);
-//      cat.tenCatsWithDefaultFields();
-        cat.tenCatsWithFori();
 
+//      cat.tenCatsWithDefaultFields();
+//        cat.tenCatsWithFori();
+        cat.tenCatsDoWhile();
     }
 
 }
