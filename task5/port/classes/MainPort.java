b71c475387d9e1b4ad8c923c74fbe5b3c53acc17
dsfaceroll_laptop <frnx.set@gmail.com>
2021-02-07 21:13:30 +0300

    release (without bugfix and beauty)

--- task5/port/classes/MainPort.java
+++ task5/port/classes/MainPort.java
@@ -3,7 +3,7 @@ package task5.port.classes;
 public class MainPort {
     public static void main(String[] args) {
         ShipsAreComing port = new ShipsAreComing();
-        DiceRoll roll = new DiceRoll();
+
         port.arrival();
     }
 }
