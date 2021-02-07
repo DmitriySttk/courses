6edf061362ef889a25099bcd7b66c91eb2a1e35b
dsfaceroll_laptop <frnx.set@gmail.com>
2021-02-07 21:42:32 +0300

    release (without bugfix and beauty)

--- task5/port/classes/MainPort.java
+++ task5/port/classes/MainPort.java
@@ -4,6 +4,6 @@ public class MainPort {
     public static void main(String[] args) {
         ShipsAreComing port = new ShipsAreComing();
 
-        port.arrival();
+        port.show(port.arrival());
     }
 }
--- task5/port/classes/ShipsAreComing.java
+++ task5/port/classes/ShipsAreComing.java
@@ -1,21 +1,27 @@
 package task5.port.classes;
 
 public class ShipsAreComing extends ShipWeight {
-    public void arrival() {
+    public double[] arrival() {
         double[] ships = new double[10];
 
-        for (int i = 0; i <ships.length; i++) {
+        for (int i = 0; i < ships.length; i++) {
             int randomShip = rollOneToTwo();
-            if (randomShip==1){
-                ships[i]= oneDeckShipWaterWeight();
+            if (randomShip == 1) {
+                ships[i] = oneDeckShipWaterWeight();
             }
-            if (randomShip==2){
-                ships[i]=twoDecksShipWaterWeight();
+            if (randomShip == 2) {
+                ships[i] = twoDecksShipWaterWeight();
             }
         }
-        for (int i = 0; i < ships.length ; i++) {
-            System.out.println(i+1+" "+ships[i]);
-        }
+//        for (int i = 0; i < ships.length; i++) {
+//            System.out.println(i + 1 + " " + ships[i]);
+//        }
+        return ships;
+    }
 
+    public void show(double[] ships) {
+        for (int i = 0; i < ships.length; i++) {
+            System.out.println(i + 1 + " " + ships[i]);
+        }
     }
 }
