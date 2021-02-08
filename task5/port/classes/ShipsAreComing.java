cbc395843ac871d71c91a8a49c763f005d9f91d2
dsfaceroll_laptop <frnx.set@gmail.com>
2021-02-08 03:59:07 +0300

    rework total weight + cast to int

--- task5/port/classes/ShipsAreComing.java
+++ task5/port/classes/ShipsAreComing.java
@@ -1,10 +1,9 @@
 package task5.port.classes;
 
 public class ShipsAreComing extends ShipWeight {
+    double[] ships = new double[10];
+
     public double[] arrival() {
-        double[] ships = new double[10];
-
-
         for (int i = 0; i < ships.length; i++) {
             int randomShip = rollOneToTwo();
             if (randomShip == 1) {
@@ -14,24 +13,24 @@ public class ShipsAreComing extends ShipWeight {
                 ships[i] = twoDecksShipWaterWeight();
             }
         }
-//        for (int i = 0; i < ships.length; i++) {
-//            System.out.println(i + 1 + " " + ships[i]);
-//        }
         return ships;
     }
 
     public void show(double[] ships) {
         for (int i = 0; i < ships.length; i++) {
-            System.out.println(i + 1 + " " + ships[i]);
+            System.out.println(i + 1 + " ship contains " + ships[i]);
         }
         System.out.println();
     }
 
     public void showTotalWeight(double[] ships) {
-        double totalResult = 0;
+        int totalResult = 0;
+        double totalResultDouble = 0;
         for (int i = 0; i < ships.length; i++) {
-            totalResult = totalResult+ships[i];
+            totalResult = (int) (totalResult + ships[i]);
+            totalResultDouble = (totalResultDouble + ships[i]);
         }
-        System.out.println("total weight = "+totalResult);
+        System.out.println("total weight = " + totalResultDouble);
+        System.out.println("total weight cast to int = " + totalResult);
     }
 }
