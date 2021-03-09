8058df1dc1293b32438db18a59da8b453db07340
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-03-09 16:08:32 +0300

    add tests for cylinder and cuboid volumes

--- task8/task5withtests/tests/ContainerVolumeTest.java
+++ task8/task5withtests/tests/ContainerVolumeTest.java
@@ -9,8 +9,16 @@ public class ContainerVolumeTest {
     ContainerVolume volume = new ContainerVolume();
 
     @Test
-    void whenSixAndEightThenHundredConeVolume(){
+    void when6And8Then100ConeVolume(){
         Assertions.assertEquals(100.53096491487338,volume.coneVolume(6,8));
+    }
+    @Test
+    void when6And8Then192CuboidVolume(){
+        Assertions.assertEquals(192.0,volume.cuboidVolume(6,8));
+    }
+    @Test
+    void when6And8Then300CylinderVolume(){
+        Assertions.assertEquals(301.59289474462014,volume.cylinderVolume(6,8));
     }
 
 
