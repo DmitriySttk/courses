f1162f41fadf8bf663b7bd0c599260739c6235ed
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-03-09 17:04:10 +0300

    add ContainerWeightTest

---
+++ task8/task5withtests/tests/ContainerWeightTest.java
@@ -0,0 +1,19 @@
+package task8.task5withtests.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import task8.task5withtests.classes.ContainerWeight;
+
+public class ContainerWeightTest {
+    ContainerWeight weight = new ContainerWeight();
+
+    @Test
+    void when1then1000LightContainerWeight(){
+        Assertions.assertEquals(1000, weight.lightContainerWeight(1));
+    }
+
+    @Test
+    void when1then2000HeavyContainerWeight(){
+        Assertions.assertEquals(2000, weight.heavyContainerWeight(1));
+    }
+}
