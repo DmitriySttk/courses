f27aa15f46e6cafd88cac9e82dba8a563e5fb809
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-03-15 14:57:10 +0300

    int commit

---
+++ task9/exceptions/ExceptionsMain.java
@@ -0,0 +1,10 @@
+package task9.exceptions;
+
+public class ExceptionsMain {
+    public static void main(String[] args) {
+        SimpleClass area = new SimpleClass();
+        System.out.println(area.area(2, -3));
+
+
+    }
+}
---
+++ task9/exceptions/SimpleClass.java
@@ -0,0 +1,12 @@
+package task9.exceptions;
+
+public class SimpleClass {
+
+    public static int area(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Negative sizes: w = " + width + ", h = " + height);
+        }
+        return width * height;
+    }
+
+}
