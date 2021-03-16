84d1dbc8f6c4d73505907c40f10a0d7890b3cee5
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-03-16 16:37:43 +0300

    add divide by odd number exception

--- task9/exceptions/ExceptionsMain.java
+++ task9/exceptions/ExceptionsMain.java
@@ -1,9 +1,9 @@
 package task9.exceptions;
 
 public class ExceptionsMain {
-    public static void main(String[] args) throws SimpleClassException {
+    public static void main(String[] args) {
         SimpleClass area = new SimpleClass();
-        System.out.println(area.area(2, -3));
+        System.out.println(area.area(2, 3));
 
         SimpleClass devide = new SimpleClass();
         try{
