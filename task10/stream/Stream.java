d6a229913a8527b1c42a87bad1fccc6f88727576
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-03-17 11:43:55 +0300

    init commit

---
+++ task10/stream/Stream.java
@@ -0,0 +1,9 @@
+package task10.stream;
+
+public class Stream {
+    public void col(){
+        Cpu sempron = new Cpu("Sempron",1.5);
+        Cpu athlon = new Cpu("Athlon",2.5);
+
+    }
+}
--- task10/stream/StreamMain.java
+++ task10/stream/StreamMain.java
@@ -2,7 +2,6 @@ package task10.stream;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Cpu sempron = new Cpu("Sempron",1.5);
 
     }
 
