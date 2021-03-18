980023554308a93f783822c761e081c9752a96df
Faceroll17 <59966721+Dmitry.St@users.noreply.github.com>
2021-03-18 17:24:31 +0300

    test method

--- task10/stream/StreamApiMethods.java
+++ task10/stream/StreamApiMethods.java
@@ -7,18 +7,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamApiMethods {
+    Collection<Cpu> CpuList = Arrays.asList(new Cpu("Athlon", 1.5),
+            new Cpu("Celeron", 1.5),
+            new Cpu("Sempron", 1.5),
+            new Cpu("Athlon", 3.5),
+            new Cpu("Intel", 2),
+            new Cpu("Atmega8", 0.001),
+            new Cpu("STM32", 0.48),
+            new Cpu("Ryzen", 3.5),
+            new Cpu("Intel", 3),
+            new Cpu("Athlon", 3));
 
-    public void streamApi(){
-        Collection<Cpu> CpuList = Arrays.asList(new Cpu("Athlon", 1.5),
-                new Cpu("Celeron", 1.5),
-                new Cpu("Sempron", 1.5),
-                new Cpu("Athlon", 3.5),
-                new Cpu("Intel", 2),
-                new Cpu("Atmega8", 0.001),
-                new Cpu("STM32", 0.48),
-                new Cpu("Ryzen", 3.5),
-                new Cpu("Intel", 3),
-                new Cpu("Athlon", 3));
+    public void streamTest(){
 
         List<Cpu> test = CpuList.stream().filter((p)->p.getFrequency()>=2).collect(Collectors.toList());
         System.out.println("2+ GHz: " +test );
--- task10/stream/StreamMain.java
+++ task10/stream/StreamMain.java
@@ -1,11 +1,9 @@
 package task10.stream;
 
-import java.util.Collection;
-
 public class StreamMain {
     public static void main(String[] args) {
         StreamApiMethods test = new StreamApiMethods();
-        test.streamApi();
+        test.streamTest();
     }
 
 }
